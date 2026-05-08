b6707f0399dbbe40fc8b4b998c236a5e586314fd
Universe <universe@grida.co>
2026-05-08 20:04:11 +0900

    fix(log): align LogarithmIsland CodeMap to arc4 rename
    Pre-push astro check caught the missing rename in the island wrapper.

--- apps/web/src/views/LogarithmIsland.tsx
+++ apps/web/src/views/LogarithmIsland.tsx
@@ -1,7 +1,7 @@
 import { AppProvider } from "../context/AppContext";
 import { Logarithm } from "./Logarithm";
 
-export default function LogarithmIsland({ code }: { code: { arc1: string; arc3: string } }) {
+export default function LogarithmIsland({ code }: { code: { arc1: string; arc4: string } }) {
   return (
     <AppProvider>
       <Logarithm code={code} />
